0541704d8d705b5c25936dcc1b7d7590258c8a52
umbara-web <umbara81.web@gmail.com>
2026-03-30 12:48:54 +0700

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -36,7 +36,7 @@ export const Header = () => {
         className='bg-transparent fixed z-20 w-full border-b border-gray-500 backdrop-blur-3xl'
       >
         <div className='mx-auto max-w-6xl px-4 transition-all duration-300'>
-          <div className='relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4'>
+          <div className='relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-2'>
             {/* Logo */}
             <div className='flex w-full items-center justify-between gap-12 lg:w-auto'>
               <a
@@ -65,7 +65,7 @@ export const Header = () => {
 
               {/* Navigation Menu Dekstop */}
               <div className='hidden lg:block'>
-                <nav className='flex flex-wrap justify-between items-center font-bold'>
+                <nav className='flex flex-wrap justify-center items-center font-bold'>
                   <NavigationMenu />
                 </nav>
               </div>
